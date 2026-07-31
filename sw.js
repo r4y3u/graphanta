83d19d2b90f709ddc0ed033c697298a1d59431c3
r4y3u <gorozetto@yahoo.co.jp>
2026-07-31 22:32:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'graphanta-runtime-v0.4.0-alpha.3';
+const CACHE = 'graphanta-runtime-v0.5.0-alpha.1';
 const ROOT = new URL('./', self.registration.scope).toString();
 
 async function precacheAppShell() {
